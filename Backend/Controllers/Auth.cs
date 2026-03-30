7312d8cb1af0d6fa76743549dabedbfe06d42fb4
stinahaziri <stinna.haziri@gmail.com>
2026-03-30 21:44:17 +0200

    AuthController

---
+++ Backend/Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class Auth : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
